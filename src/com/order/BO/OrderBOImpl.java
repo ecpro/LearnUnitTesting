c96362141c61be1a66fc76e81fde0a8689e1437c
Piyush Ravi <mails4ipiyush@gmail.com>
2017-04-05 23:43:40 +0530

    mockito example

--- src/com/order/BO/OrderBOImpl.java
+++ src/com/order/BO/OrderBOImpl.java
@@ -1,26 +1,59 @@
 package com.order.BO;
 
+import java.sql.SQLException;
+
 import com.order.BO.exception.BOException;
+import com.order.DAO.OrderDAO;
 import com.order.dto.Order;
 
 public class OrderBOImpl implements OrderBO {
+	
+	private OrderDAO dao;
 
 	@Override
 	public boolean placeOrder(Order order) throws BOException {
-		// TODO Auto-generated method stub
-		return false;
+		
+		int result = 0;
+		try {
+			result = dao.create(order);
+		} catch (SQLException e) {
+			throw new BOException(e);
+		}
+		
+		return result > 0 ? true:false;
 	}
 
 	@Override
 	public boolean cancelOrder(int id) throws BOException {
-		// TODO Auto-generated method stub
-		return false;
+		int result = 0;
+		try {
+			 Order order = dao.read(id);
+			 order.setStatus("cancel");
+			 result = dao.update(order);
+		} catch (SQLException e) {
+			throw new BOException(e);
+		}
+		return result > 0 ? true : false;
 	}
 
 	@Override
 	public boolean deleteOrder(int id) throws BOException {
-		// TODO Auto-generated method stub
-		return false;
+		
+		int result = 0;
+		try {
+			result = dao.delete(id);
+		} catch (SQLException e) {
+			throw new BOException(e);
+		}
+		return result > 0 ? true : false;
+	}
+
+	public OrderDAO getDao() {
+		return dao;
+	}
+
+	public void setDao(OrderDAO dao) {
+		this.dao = dao;
 	}
 
 }
--- src/com/order/BO/exception/BOException.java
+++ src/com/order/BO/exception/BOException.java
@@ -1,5 +1,16 @@
 package com.order.BO.exception;
 
+import java.sql.SQLException;
+
 public class BOException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BOException(SQLException e) {
+		super(e);
+	}
 
 }
